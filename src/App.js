99a154d5ac495f4e5e8f782062cf1aa35ca62322
noahblob <noahhagardent@gmail.com>
2023-07-20 12:17:21 +1200

    Added scroll indicator at top of page

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import Home from './components/Home';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import ScrollIndicator from './components/ScrollIndicator';
 
 export default function App() {
   return (
     <main>
+      <ScrollIndicator />
       <div className='container'>
         <Home />
         <About />
