9aef40d53ad02c1158e1403d0099333768a77148
noahblob <noahhagardent@gmail.com>
2023-07-21 12:47:30 +1200

    Added a fade effect on scroll using react-reveal

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,7 @@
 import Tooltip from 'rc-tooltip';
 import 'rc-tooltip/assets/bootstrap.css';
+import Fade from 'react-reveal/Fade';
+
 
 import profile from '../resources/profile-pic.jpg';
 
@@ -13,38 +15,40 @@ export default function About() {
   return (
     <div className='fullscreen'>
       <div className='container-fluid text-center'>
-        <div className='row'>
-          <div className='col-lg my-auto'>
-            <img src={profile} alt='Profile' className='img-fluid rounded-circle p-5' height='500' width='500' />
-            <h2>About Me</h2>
-            <p>I'm an aspiring Software Engineer based in Auckland, New Zealand. I am passionate about front-end and back-end development and I consistently strive to do my best in all areas of my life: Personal, professional and academic.</p>
+        <Fade Top>
+          <div className='row'>
+            <div className='col-lg my-auto'>
+              <img src={profile} alt='Profile' className='img-fluid rounded-circle p-5' height='500' width='500' />
+              <h2>About Me</h2>
+              <p>I'm an aspiring Software Engineer based in Auckland, New Zealand. I am passionate about front-end and back-end development and I consistently strive to do my best in all areas of my life: Personal, professional and academic.</p>
+            </div>
+            <div className='col-lg my-auto'>
+              <h2>My Tech Stack</h2>
+
+              <Tooltip placement='bottom' overlay={<span>Java</span>}>
+                <img src={javaicon} alt='Java' className='icon img-fluid p-2' />
+              </Tooltip>
+
+              <Tooltip placement='bottom' overlay={<span>HTML</span>}>
+                <img src={htmlicon} alt='HTML' className='icon img-fluid p-2' />
+              </Tooltip>
+
+              <Tooltip placement='bottom' overlay={<span>CSS</span>}>
+                <img src={cssicon} alt='CSS' className='icon img-fluid p-2' />
+              </Tooltip>
+
+              <Tooltip placement='bottom' overlay={<span>JavaScript</span>}>
+                <img src={javascripticon} alt='JavaScript' className='icon img-fluid p-2' />
+              </Tooltip>
+
+              <Tooltip placement='bottom' overlay={<span>Python</span>}>
+                <img src={pythonicon} alt='Python' className='icon img-fluid p-2' />
+              </Tooltip>
+
+              <p>Being relatively new to software engineering, I am always looking for new ways to challenge myself and learn new programming skills. I look to expand my tech stack further in the future as well as hone my current skills.</p>
+            </div>
           </div>
-          <div className='col-lg my-auto'>
-            <h2>My Tech Stack</h2>
-
-            <Tooltip placement='bottom' overlay={<span>Java</span>}>
-              <img src={javaicon} alt='Java' className='icon img-fluid p-2' />
-            </Tooltip>
-
-            <Tooltip placement='bottom' overlay={<span>HTML</span>}>
-              <img src={htmlicon} alt='HTML' className='icon img-fluid p-2' />
-            </Tooltip>
-
-            <Tooltip placement='bottom' overlay={<span>CSS</span>}>
-              <img src={cssicon} alt='CSS' className='icon img-fluid p-2' />
-            </Tooltip>
-
-            <Tooltip placement='bottom' overlay={<span>JavaScript</span>}>
-              <img src={javascripticon} alt='JavaScript' className='icon img-fluid p-2' />
-            </Tooltip>
-
-            <Tooltip placement='bottom' overlay={<span>Python</span>}>
-              <img src={pythonicon} alt='Python' className='icon img-fluid p-2' />
-            </Tooltip>
-
-            <p>Being relatively new to software engineering, I am always looking for new ways to challenge myself and learn new programming skills. I look to expand my tech stack further in the future as well as hone my current skills.</p>
-          </div>
-        </div>
+        </Fade>
       </div>
     </div>
   )
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,11 @@
+import Fade from 'react-reveal/Fade';
+
 export default function Home() {
   return (
     <div className='fullscreen w-100'>
-      <h1 className='h1 text-white text-left w-100'>Hi there! I'm <strong>Noah Hagar-Dent</strong>, a Software Engineering student at the University of Auckland.</h1>
+      <Fade top>
+        <h1 className='h1 text-white text-left w-100'>Hi there! I'm <strong>Noah Hagar-Dent</strong>, a Software Engineering student at the University of Auckland.</h1>
+      </Fade>
       <ul className='background'>
         <li></li>
         <li></li>
