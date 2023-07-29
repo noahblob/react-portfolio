46592241ad23797c362fcb3ac64f61e327ede5d7
noahblob <noahhagardent@gmail.com>
2023-07-29 15:56:41 +1200

    added custom cursor

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 
-import React from 'react';
+import { useEffect, useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 import Home from './components/Home';
 import About from './components/About';
@@ -11,12 +12,38 @@ import ScrollIndicator from './components/ScrollIndicator';
 import TopButton from './components/TopButton';
 
 export default function App() {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  console.log(mousePosition);
+
+  useEffect(() => {
+    const mouseMove = (e) => {
+      console.log(e);
+      setMousePosition({ x: e.clientX, y: e.clientY });
+    }
+    window.addEventListener('mousemove', mouseMove);
+
+    return () => { window.removeEventListener('mousemove', mouseMove); }
+  }, []);
+
+  const variants = {
+    default: {
+      x: mousePosition.x - 10,
+      y: mousePosition.y - 10
+    }
+  }
+
   return (
     <BrowserRouter>
       <main>
         <ScrollIndicator />
         <TopButton />
         <div className='container-fluid'>
+          <motion.div
+            className='cursor'
+            variants={variants}
+            animate='default'
+          />
           <Home />
           <About />
           <Projects />
